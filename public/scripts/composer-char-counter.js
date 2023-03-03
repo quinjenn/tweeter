75fc37364a6a41ff315483c5a5541cd97dfe00a8
quinjenn <jquintal_@hotmail.com>
2023-03-03 00:21:10 -0800

    final touches

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
   const maxCharLength = 140;
   // --- our code goes here ---
-  // console.log("Is this running accurately?")
   // character count for tweet form
   $("#tweet-text").on("input", function (event) {
     let tweetLength = $(this).val().length;
-    //  console.log("tweetLength", tweetLength);
     // subtract character number
     let charRemaining = maxCharLength - $(this).val().length;
-    //  console.log("charRemaining", charRemaining)
     if (tweetLength > maxCharLength) {
       // change count to red if over character limit
       $(".counter").addClass("negative-counter");
